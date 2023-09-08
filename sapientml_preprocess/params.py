06b1edc4b1bedf6b0e916fe10b4f4da876101da3
sun-ming-fujitsu <sun.ming@fujitsu.com>
2023-09-08 10:45:11 +0900

    docs:Adding docstrings to params.py
    Signed-off-by: sun-ming-fujitsu <sun.ming@fujitsu.com>

--- sapientml_preprocess/params.py
+++ sapientml_preprocess/params.py
@@ -4,5 +4,19 @@ from sapientml.params import Config
 
 
 class PreprocessConfig(Config):
+    """PreprocessConfig.
+
+    Attributes
+    ----------
+    use_pos_list : Optional[list[str]]
+      List of parts-of-speech to be used during text analysis.
+      This variable is used for japanese texts analysis.
+      Select the part of speech below.
+      "名詞", "動詞", "形容詞", "形容動詞", "副詞".
+    use_word_stemming : bool default True
+      Specify whether or not word stemming is used.
+      This variable is used for japanese texts analysis.
+
+    """
     use_pos_list: Optional[list[str]] = ["名詞", "動詞", "助動詞", "形容詞", "副詞"]
     use_word_stemming: bool = True
